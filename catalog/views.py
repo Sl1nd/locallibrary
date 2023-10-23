a0c6cedabd2965bb6bd0bb936009aa449d02d66a
Sl1nd <s_lindauer@t-online.de>
2023-10-23 06:09:47 +0200

    feat: using session framework to display number of visits

--- catalog/views.py
+++ catalog/views.py
@@ -4,12 +4,15 @@ from catalog.models import Book, BookInstance, Author
 
 # Create your views here.
 def index(request):
-
+    
     num_books: Book = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
@@ -19,6 +22,7 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
